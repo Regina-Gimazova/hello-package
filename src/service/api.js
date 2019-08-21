bb8c6667b0ad23cc01571df23963ec479a57071a
Anton Chaynikov <anton.chaynikov@flatstack.com>
2019-08-21 14:32:35 +0300

    Updated api, moved to the service dir

---
+++ src/service/api.js
@@ -0,0 +1,16 @@
+
+import axios from 'axios';
+
+class Api {
+  constructor(baseUrl) {
+    this.axios = axios.create({ baseURL: baseUrl, headers: {'Accept': 'application/json'}})
+  }
+
+  getPendingCallsList() {
+    return this.axios.get('support/rooms/active')
+      .then(response => response.data);
+  }
+}
+
+const api = new Api('https://private-1363b0-hellopackageapi.apiary-mock.com/v1');
+export default api;
